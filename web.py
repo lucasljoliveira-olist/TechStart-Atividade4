b30464748738cc0dc5761c42da40cce5371fb909
lucasljoliveira <lucasljoliveira@hotmail.com>
2020-12-23 14:49:07 -0300

    Adição de templates html e estilo css

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from MarketPlaces import Marketplace
 from Categories import Category, SubCategory
 from main import *
@@ -8,37 +8,19 @@ from main import *
 if __name__ == '__main__':
     app = Flask(__name__)
 
-    @app.route('/marketplacecategory/<marketplace>')
+    @app.route('/category/<marketplace>')
     def list_marketplace_category(marketplace) -> None:
-        ol = '<ul>'
-        h1 = '<h1> Categorias </h1>'
-        for m in get_marketplaces():
-            if m.get_id() == int(marketplace):
-                for c in m.get_categories():
-                    ol += "<li><a href = '/listsubcategory/" + str(c.get_id()) + "'>" + c.get_name()+ "</a></li> </br>"
-
-        ol += '<ul>'
-        voltar = '<a href="/">Voltar!</a>'
-        return h1 + ol + voltar
+        titulo = 'Categorias'
+        return render_template('categories.html', marketplaces = get_marketplaces(), tittle = titulo, marketplace = int(marketplace))
 
     @app.route('/')
     def index() -> None:
-        h1 = '<h1> Marketplaces </h1>'
-        ol = '<ul>'
-        for c in get_marketplaces():
-            ol += "<li><a href = '/marketplacecategory/" + str(c.get_id()) + "'>" + c.get_name()+ "</a></li> </br>"
-        ol += '<ul>'
-        return h1 + ol
+        titulo = 'Marketplaces'
+        return render_template('marketplaces.html', marketplaces = get_marketplaces(), tittle = titulo)
 
     @app.route('/listsubcategory/<category>')
     def list_subcategory(category) -> None:
-        ol = '<ul>'
-        h1 = '<h1> Subcategorias </h1>'
-        for sc in get_subcategories():
-            if sc.get_parent().get_id() == int(category):
-                ol += "<li>" + sc.get_name()+ "</li> </br>"
-        ol += '<ul>'
-        voltar = '<a href="/">Voltar!</a>'
-        return h1 + ol + voltar
+        titulo = 'Subcategorias'
+        return render_template('subcategories.html', subcategories = get_subcategories(), tittle = titulo, category = int(category))
 
     app.run(debug = True)
